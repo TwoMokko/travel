3a73775db6ed93aae721a2d47b89c85b8a01914c
Саша <two.mokko@gmail.com>
2022-11-08 23:20:01 +0700

    Edit style sheets .h

--- about.php
+++ about.php
@@ -21,14 +21,30 @@
 
 
     function Main(): void {
-        ?><div class = "block page_about_us p">
-            <div class = "about_header">
-                <div>Маленькая зел надпись</div>
+        ?><div class = "block page_about_us">
+            <div class = "about_header block p">
                 <div>
-                    <div>Мы семья
-                        <br>тыры пыры</div>
-                    <div>Какой-то тект про нас
-                        <br>справа другим шрифтом</div>
+                    <div>что-то</div>
+                    <div>Мы такие-то такие <br>вот текст такой</div>
+                    <div>Еще какой-то текст подлиннее, например, что-то немного о создании или о миссии или о ценностях</div>
+                </div>
+                <div>picture</div>
+            </div>
+            <div></div>
+            <div class = "block p">
+                <div>
+                    <div>что-то</div>
+                    <div>Мы такие-то такие <br>вот текст такой</div>
+                    <div>Еще какой-то текст подлиннее, например, что-то немного о создании или о миссии или о ценностях</div>
+                </div>
+                <div>picture</div>
+            </div>
+            <div class = "block p">
+                <div>picture</div>
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
                 </div>
             </div>
         </div><?php
