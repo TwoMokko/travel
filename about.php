19ce0f6e3b4494393171a918456a7799005f67a7
Mokko <two.mokko@gmail.com>
2022-11-09 22:58:45 +0700

    Edit style sheets .w

--- about.php
+++ about.php
@@ -23,28 +23,33 @@
     function Main(): void {
         ?><div class = "block page_about_us">
             <div class = "about_header block p">
-                <div>
+                <div class = "about_text">
                     <div>что-то</div>
-                    <div>Мы такие-то такие <br>вот текст такой</div>
+                    <div>Организуем путешествия на основе ваших пожеланий,
+						<br>а так же по подготовленным нами маршрутам
+						<br>Организуем путешествия на основе ваших пожеланий,
+						<br>а так же по подготовленным нами маршрутам</div>
+                    <div class = "right_text">Еще какой-то текст подлиннее, например, что-то немного о создании или о миссии или о ценностях</div>
+                </div>
+                <div class = "about_pic"></div>
+            </div>
+            <div>
+				<div></div>
+			</div>
+            <div class = "block p">
+                <div class = "about_text">
+                    <div>Организатор</div>
+                    <div>Ишмурзина Нина</div>
                     <div>Еще какой-то текст подлиннее, например, что-то немного о создании или о миссии или о ценностях</div>
                 </div>
-                <div>picture</div>
-            </div>
-            <div></div>
-            <div class = "block p">
-                <div>
-                    <div>что-то</div>
-                    <div>Мы такие-то такие <br>вот текст такой</div>
-                    <div>Еще какой-то текст подлиннее, например, что-то немного о создании или о миссии или о ценностях</div>
-                </div>
-                <div>picture</div>
+                <div class = "about_pic"></div>
             </div>
             <div class = "block p">
-                <div>picture</div>
-                <div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                <div class = "about_pic"></div>
+                <div class = "about_text">
+                    <div>Водитель</div>
+                    <div>Меховников Сергей</div>
+                    <div>Супер крутое описание человека которыей чем-то занимается в этой компании</div>
                 </div>
             </div>
         </div><?php
