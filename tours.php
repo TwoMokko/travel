0cfdd418591739e61322d455643bbf1214e9a77f
Саша <two.mokko@gmail.com>
2022-12-02 23:27:06 +0700

    Edit style sheets .h

--- tours.php
+++ tours.php
@@ -21,234 +21,247 @@
 
     function Main(): void {
         ?>
-        <div class = "block page_tours p">
-            <div class = "header">Основные путешествия</div>
-            <div class = "cards_tours">
-                <div>
-                    <div></div>
-                    <div>
-                        <div>Дата</div>
-						<div>Название</div>
-						<div>Описание</div>
-                        <div>Цена</div>
+        <div class = "relative_tours">
+            <div class = "navigation_tour">
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+            </div>
+            <div>
+                <div class = "block page_tours p">
+                    <div class = "header">Основные путешествия</div>
+                    <div class = "cards_tours">
                         <div>
-<!--							<span class = "more">Подробнее ⟶</span>-->
-							<a class = "button border" href = "/tour">Подробнее</a>
-							<input type = "button" value = "Поехать">
+                            <div></div>
+                            <div>
+                                <div>Дата</div>
+                                <div>Название</div>
+                                <div>Описание</div>
+                                <div>Цена</div>
+                                <div>
+                                    <!--							<span class = "more">Подробнее ⟶</span>-->
+                                    <a class = "button border" href = "/tour">Подробнее</a>
+                                    <input type = "button" value = "Поехать">
+                                </div>
+                            </div>
                         </div>
-					</div>
+                        <div>
+                            <div></div>
+                            <div>
+                                <div>Дата</div>
+                                <div>Название</div>
+                                <div>Описание</div>
+                                <div>Цена</div>
+                                <div>
+                                    <a class = "button border" href = "/tour">Подробнее</a>
+                                    <input type = "button" value = "Поехать">
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div></div>
+                            <div>
+                                <div>Дата</div>
+                                <div>Название</div>
+                                <div>Описание</div>
+                                <div>Цена</div>
+                                <div>
+                                    <a class = "button border" href = "/tour">Подробнее</a>
+                                    <input type = "button" value = "Поехать">
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div></div>
+                            <div>
+                                <div>Дата</div>
+                                <div>Название</div>
+                                <div>Описание</div>
+                                <div>Цена</div>
+                                <div>
+                                    <a class = "button border" href = "/tour">Подробнее</a>
+                                    <input type = "button" value = "Поехать">
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div></div>
+                            <div>
+                                <div>Дата</div>
+                                <div>Название</div>
+                                <div>Описание</div>
+                                <div>Цена</div>
+                                <div>
+                                    <a class = "button border" href = "/tour">Подробнее</a>
+                                    <input type = "button" value = "Поехать">
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div></div>
+                            <div>
+                                <div>Дата</div>
+                                <div>Название</div>
+                                <div>Описание</div>
+                                <div>Цена</div>
+                                <div>
+                                    <a class = "button border" href = "/tour">Подробнее</a>
+                                    <input type = "button" value = "Поехать">
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div></div>
+                            <div>
+                                <div>Дата</div>
+                                <div>Название</div>
+                                <div>Описание</div>
+                                <div>Цена</div>
+                                <div>
+                                    <a class = "button border" href = "/tour">Подробнее</a>
+                                    <input type = "button" value = "Поехать">
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div></div>
+                            <div>
+                                <div>Дата</div>
+                                <div>Название</div>
+                                <div>Описание</div>
+                                <div>Цена</div>
+                                <div>
+                                    <a class = "button border" href = "/tour">Подробнее</a>
+                                    <input type = "button" value = "Поехать">
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div></div>
+                            <div>
+                                <div>Дата</div>
+                                <div>Название</div>
+                                <div>Описание</div>
+                                <div>Цена</div>
+                                <div>
+                                    <a class = "button border" href = "/tour">Подробнее</a>
+                                    <input type = "button" value = "Поехать">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <div></div>
-					<div>
-                        <div>Дата</div>
-						<div>Название</div>
-						<div>Описание</div>
-                        <div>Цена</div>
+                <div class = "block page_tours p">
+                    <div class = "header">Индивидуальное сопровождение</div>
+                    <div class = "cards_tours">
                         <div>
-							<a class = "button border" href = "/tour">Подробнее</a>
-                            <input type = "button" value = "Поехать">
+                            <div></div>
+                            <div>
+                                <div>Дата</div>
+                                <div>Название</div>
+                                <div>Описание</div>
+                                <div>Цена</div>
+                                <div>
+                                    <a class = "button border" href = "/tour">Подробнее</a>
+                                    <input type = "button" value = "Поехать">
+                                </div>
+                            </div>
                         </div>
-					</div>
+                        <div>
+                            <div></div>
+                            <div>
+                                <div>Дата</div>
+                                <div>Название</div>
+                                <div>Описание</div>
+                                <div>Цена</div>
+                                <div>
+                                    <a class = "button border" href = "/tour">Подробнее</a>
+                                    <input type = "button" value = "Поехать">
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div></div>
+                            <div>
+                                <div>Дата</div>
+                                <div>Название</div>
+                                <div>Описание</div>
+                                <div>Цена</div>
+                                <div>
+                                    <a class = "button border" href = "/tour">Подробнее</a>
+                                    <input type = "button" value = "Поехать">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <div></div>
-					<div>
-                        <div>Дата</div>
-						<div>Название</div>
-						<div>Описание</div>
-                        <div>Цена</div>
+                <div class = "block page_tours p">
+                    <div class = "header">ВИП</div>
+                    <div class = "cards_tours">
                         <div>
-							<a class = "button border" href = "/tour">Подробнее</a>
-                            <input type = "button" value = "Поехать">
+                            <div></div>
+                            <div>
+                                <div>Дата</div>
+                                <div>Название</div>
+                                <div>Описание</div>
+                                <div>Цена</div>
+                                <div>
+                                    <a class = "button border" href = "/tour">Подробнее</a>
+                                    <input type = "button" value = "Поехать">
+                                </div>
+                            </div>
                         </div>
-					</div>
+                        <div>
+                            <div></div>
+                            <div>
+                                <div>Дата</div>
+                                <div>Название</div>
+                                <div>Описание</div>
+                                <div>Цена</div>
+                                <div>
+                                    <a class = "button border" href = "/tour">Подробнее</a>
+                                    <input type = "button" value = "Поехать">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <div></div>
-					<div>
-                        <div>Дата</div>
-						<div>Название</div>
-						<div>Описание</div>
-                        <div>Цена</div>
+                <div class = "block page_tours p">
+                    <div class = "header">Выходного дня</div>
+                    <div class = "cards_tours">
                         <div>
-							<a class = "button border" href = "/tour">Подробнее</a>
-                            <input type = "button" value = "Поехать">
+                            <div></div>
+                            <div>
+                                <div>Дата</div>
+                                <div>Название</div>
+                                <div>Описание</div>
+                                <div>Цена</div>
+                                <div>
+                                    <a class = "button border" href = "/tour">Подробнее</a>
+                                    <input type = "button" value = "Поехать">
+                                </div>
+                            </div>
                         </div>
-					</div>
-                </div>
-                <div>
-                    <div></div>
-					<div>
-                        <div>Дата</div>
-						<div>Название</div>
-						<div>Описание</div>
-                        <div>Цена</div>
                         <div>
-							<a class = "button border" href = "/tour">Подробнее</a>
-                            <input type = "button" value = "Поехать">
+                            <div></div>
+                            <div>
+                                <div>Дата</div>
+                                <div>Название</div>
+                                <div>Описание</div>
+                                <div>Цена</div>
+                                <div>
+                                    <a class = "button border" href = "/tour">Подробнее</a>
+                                    <input type = "button" value = "Поехать">
+                                </div>
+                            </div>
                         </div>
-					</div>
-                </div>
-                <div>
-                    <div></div>
-					<div>
-                        <div>Дата</div>
-						<div>Название</div>
-						<div>Описание</div>
-                        <div>Цена</div>
-                        <div>
-							<a class = "button border" href = "/tour">Подробнее</a>
-                            <input type = "button" value = "Поехать">
-                        </div>
-					</div>
-                </div>
-                <div>
-                    <div></div>
-					<div>
-                        <div>Дата</div>
-						<div>Название</div>
-						<div>Описание</div>
-                        <div>Цена</div>
-                        <div>
-							<a class = "button border" href = "/tour">Подробнее</a>
-                            <input type = "button" value = "Поехать">
-                        </div>
-					</div>
-                </div>
-                <div>
-                    <div></div>
-					<div>
-                        <div>Дата</div>
-						<div>Название</div>
-						<div>Описание</div>
-                        <div>Цена</div>
-                        <div>
-							<a class = "button border" href = "/tour">Подробнее</a>
-                            <input type = "button" value = "Поехать">
-                        </div>
-					</div>
-                </div>
-                <div>
-                    <div></div>
-					<div>
-                        <div>Дата</div>
-						<div>Название</div>
-						<div>Описание</div>
-                        <div>Цена</div>
-                        <div>
-							<a class = "button border" href = "/tour">Подробнее</a>
-                            <input type = "button" value = "Поехать">
-                        </div>
-					</div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class = "block page_tours p">
-            <div class = "header">Индивидуальное сопровождение</div>
-            <div class = "cards_tours">
-                <div>
-                    <div></div>
-					<div>
-                        <div>Дата</div>
-						<div>Название</div>
-						<div>Описание</div>
-                        <div>Цена</div>
-						<div>
-							<a class = "button border" href = "/tour">Подробнее</a>
-                            <input type = "button" value = "Поехать">
-						</div>
-					</div>
-                </div>
-				<div>
-					<div></div>
-					<div>
-						<div>Дата</div>
-						<div>Название</div>
-						<div>Описание</div>
-						<div>Цена</div>
-						<div>
-							<a class = "button border" href = "/tour">Подробнее</a>
-							<input type = "button" value = "Поехать">
-						</div>
-					</div>
-				</div>
-				<div>
-					<div></div>
-					<div>
-						<div>Дата</div>
-						<div>Название</div>
-						<div>Описание</div>
-						<div>Цена</div>
-						<div>
-							<a class = "button border" href = "/tour">Подробнее</a>
-							<input type = "button" value = "Поехать">
-						</div>
-					</div>
-				</div>
-            </div>
-        </div>
-        <div class = "block page_tours p">
-            <div class = "header">ВИП</div>
-            <div class = "cards_tours">
-				<div>
-					<div></div>
-					<div>
-						<div>Дата</div>
-						<div>Название</div>
-						<div>Описание</div>
-						<div>Цена</div>
-						<div>
-							<a class = "button border" href = "/tour">Подробнее</a>
-							<input type = "button" value = "Поехать">
-						</div>
-					</div>
-				</div>
-				<div>
-					<div></div>
-					<div>
-						<div>Дата</div>
-						<div>Название</div>
-						<div>Описание</div>
-						<div>Цена</div>
-						<div>
-							<a class = "button border" href = "/tour">Подробнее</a>
-							<input type = "button" value = "Поехать">
-						</div>
-					</div>
-				</div>
-            </div>
-        </div>
-		<div class = "block page_tours p">
-			<div class = "header">Выходного дня</div>
-			<div class = "cards_tours">
-				<div>
-					<div></div>
-					<div>
-						<div>Дата</div>
-						<div>Название</div>
-						<div>Описание</div>
-						<div>Цена</div>
-						<div>
-							<a class = "button border" href = "/tour">Подробнее</a>
-							<input type = "button" value = "Поехать">
-						</div>
-					</div>
-				</div>
-				<div>
-					<div></div>
-					<div>
-						<div>Дата</div>
-						<div>Название</div>
-						<div>Описание</div>
-						<div>Цена</div>
-						<div>
-							<a class = "button border" href = "/tour">Подробнее</a>
-							<input type = "button" value = "Поехать">
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+        <script>
+            NavigationTour('.navigation_tour', 200, 20);
+        </script>
         <?php
     }
--- ts/main.ts
+++ ts/main.ts
@@ -1,7 +1,17 @@
-class Travel {
+function NavigationTour(selector: string, top_absolute: number, top_fixed: number) {
+	let $document = $(document);
+	let $selector = $(selector);
+	let $parent = $selector.parent();
 
-	constructor() {
+	$document.on('scroll', Redraw);
 
+	function Redraw() {
+		let _top = $parent.offset().top;
+		let _scroll = $document.scrollTop();
+
+		if (_top + (top_absolute - top_fixed) <= _scroll) $selector.css({position: 'fixed', top: `${top_fixed}px`});
+		else $selector.css({position: 'absolute', top: `${top_absolute}px`});
 	}
 
+	Redraw();
 }
